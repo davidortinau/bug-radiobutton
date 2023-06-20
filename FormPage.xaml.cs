d4b4df385b89c08cd8443314c746b0ffa11edaee
David Ortinau <david.ortinau@microsoft.com>
2023-06-20 11:20:50 -0500

    the repro

---
+++ FormPage.xaml.cs
@@ -0,0 +1,12 @@
+﻿using CommunityToolkit.Maui.Views;
+
+namespace MauiApp1;
+
+public partial class FormPage : Popup
+{
+	public FormPage()
+	{
+		InitializeComponent();
+	}
+
+}
---
+++ MainPage.xaml.cs
@@ -0,0 +1,20 @@
+﻿using CommunityToolkit.Maui.Views;
+namespace MauiApp1;
+
+public partial class MainPage : ContentPage
+{
+	public MainPage()
+	{
+		InitializeComponent();
+	}
+
+	private async void OnButtonClicked(object sender, EventArgs e)
+	{
+		var popup = new FormPage();        
+		// this.ShowPopup(popup);
+        await App.Current.MainPage.ShowPopupAsync(popup);
+	}
+
+	
+
+}
